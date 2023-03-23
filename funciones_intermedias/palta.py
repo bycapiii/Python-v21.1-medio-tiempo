5a7dd2d582789b044e606db266c47b67219232fb
qfms12 <aaron.alvarez.munoz@alumnos.sip.cl>
2023-03-23 13:03:50 -0300

    a

---
+++ funciones_intermedias/palta.py
@@ -0,0 +1,17 @@
+x = [ [5,2,3], [10,8,9] ] 
+x[1][0] = 15
+estudiantes = [
+    {'first_name':  'Michael', 'last_name' : 'Bryant'},
+    {'first_name' : 'John', 'last_name' : 'Rosales'}
+]
+directorio_deportes = {
+    'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
+    'fútbol' : ['Andres', 'Ronaldo', 'Rooney']
+}
+z = [ {'x': 10, 'y': 30} ]
+print (x)
+print(estudiantes)
+print(directorio_deportes)
+print(z)
+
+
