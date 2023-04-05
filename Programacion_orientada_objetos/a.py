75750418533e5a4122692625bb369bcc11dc03ae
qfms12 <aaron.alvarez.munoz@alumnos.sip.cl>
2023-04-05 12:44:48 -0400

    a

---
+++ Programacion_orientada_objetos/a.py
@@ -0,0 +1,2 @@
+color_favorito = input('¿Cuál es tu color favorito? ') # la entrada toma un prompt, que debe ser una cadena
+print(f'Tu color favorito is: {"rojo"}') # salida, imprime el color dado en la consola 
